8edb4b46bc88470c663bccd6fed6e24498315f1c
VineethSajjan <vineethsajjan7736@gmail.com>
2023-07-30 10:51:45 +0530

    .

---
+++ Test.java
@@ -0,0 +1,14 @@
+class Test{
+    public static void main(String[] args) {
+        Vineeth d = new Vineeth();
+        d.find();
+    }
+}
+class Vineeth{
+    Vineeth(){
+        System.out.println("constructor invoked");
+    }
+    void find(){
+        
+    }
+}
